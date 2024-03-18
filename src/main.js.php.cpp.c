b097cd74dfdf693ec0634c58fb7ef37014dd9b47
aer-i <mateusz.kallus@zslt.klucznet.pl>
2024-03-18 15:45:55 +0100

    Added connection type selection

--- src/main.js.php.cpp.c
+++ src/main.js.php.cpp.c
@@ -2,23 +2,88 @@
 #include "raylib.h"
 #include "enet.h"
 
-int 
-main
-(
-int argc,
-char** argv
-) {
+typedef unsigned ConnectionType;
+enum ConnectionTypeBits
+{
+    CONNECTION_TYPE_NONE   = 0,
+    CONNECTION_TYPE_CLIENT = 1,
+    CONNECTION_TYPE_SERVER = 2
+};
+
+ConnectionType selectConnectionType();
+void clientLoop();
+void serverLoop();
+
+int main(int argc, char** argv)
+{
     InitWindow(800, 600, "Simple multipayer game");
 
-    while (!WindowShouldClose())
+    switch (selectConnectionType())
+    {
+    case CONNECTION_TYPE_CLIENT:
+        clientLoop();
+        break;
+    case CONNECTION_TYPE_SERVER:
+        serverLoop();
+        break;
+    default:
+        break;
+    }
+
+    CloseWindow();
+}
+
+ConnectionType selectConnectionType()
+{
+    ConnectionType connectionType = CONNECTION_TYPE_NONE;
+
+    while (!WindowShouldClose() && !connectionType)
     {
         BeginDrawing();
         {
-            ClearBackground(RED);
+            ClearBackground(BLACK);
+            DrawFPS(10, 10);
+            DrawText("Select client or server [C/S]", GetRenderWidth() * 0.30, GetRenderHeight() >> 1, 20, PINK);
 
+            if (IsKeyPressed(KEY_C))
+            {
+                connectionType = CONNECTION_TYPE_CLIENT;
+            }
+            else if (IsKeyPressed(KEY_S))
+            {
+                connectionType = CONNECTION_TYPE_SERVER;
+            }
         }
         EndDrawing();
     }
 
-    CloseWindow();
+    return connectionType;
+}
+
+void clientLoop()
+{
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        {
+            ClearBackground(PINK);
+            DrawText("Clien", GetRenderWidth() >> 1, GetRenderHeight() >> 1, 20, BLACK);
+            DrawFPS(10, 10);
+        }
+        EndDrawing();
+    }
+}
+
+void serverLoop()
+{
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        {
+            ClearBackground(BLUE);
+            DrawText("Sever", GetRenderWidth() >> 1, GetRenderHeight() >> 1, 20, BLACK);
+            DrawFPS(10, 10);
+        }
+        EndDrawing();
+    }
 }
